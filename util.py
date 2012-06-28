856c260b64d3508df39310572f6968d6aa66efda
Ryan Grout <rgrout@byu.net>
2012-06-28 16:23:34 -0600

    util function to conveniently generate random observations based on possible states for the network.

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import network
+import numpy as N
+import random
 
 
 def all_parents(net, nodes):
@@ -8,3 +10,20 @@ def all_parents(net, nodes):
         return parentset.union(*[all_parents(net, net.predecessors(node)) for node in nodes])
     else:
         return set()
+        
+def genRandObs(net, nobs):
+    """generate valid random observations for net"""
+    
+    _inlut = net.graph['inlut']
+    _node = net.node
+    _choice = random.choice
+    
+    obslist = []
+    vars = len(_inlut.keys())
+    for ol in xrange(nobs):
+        ob = []
+        for vl in xrange(vars):
+            states = _node[_inlut[vl]]['states'].keys()
+            ob.append(_choice(states))
+        obslist.append(ob)
+    return obslist
