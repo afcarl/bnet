ebf286b12dcc28a65fc6a166232b6cb1176446b7
Ryan Grout <rgrout@byu.net>
2012-06-29 11:16:31 -0600

    find the most suspicious states and optionally write them to a file.

--- util.py
+++ util.py
@@ -2,7 +2,6 @@ import network
 import numpy as N
 import random
 
-
 def all_parents(net, nodes):
     #Get all the parents of nodes
     if nodes:
@@ -27,3 +26,28 @@ def genRandObs(net, nobs):
             ob.append(_choice(states))
         obslist.append(ob)
     return obslist
+    
+def suspect(jointprobs, threshold=.01, output=None, vars=[]):
+    """Find the most suspicious claims from jointprobs
+    
+    Default is least likely 1% of the claims
+    If output is defined, will write results to a file
+    """
+    
+    nprobs = jointprobs.shape[0]
+    sortedprobs = jointprobs[N.argsort(jointprobs[:,-1].astype(float))]
+    suspicious = int(nprobs*threshold)
+    suspected = sortedprobs[:suspicious]
+    if output is not None:
+        if vars:
+            with open(output, 'wb') as f:
+                f.write(','.join([str(char) for char in vars+["log prob"]]))
+                f.write('\n')
+                for obs in suspected:
+                    f.write(','.join([str(o) for o in obs]))
+                    f.write('\n')
+        else:
+            raise ValueError("to output to a file, variable names must be specified")
+    else:
+        return suspected
+    
